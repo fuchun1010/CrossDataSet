661c5e9c1f86f77159031dd62e8d39c7418df796
FuChun <fuchun198435@163.com>
2017-08-20 00:43:16 +0800

    [fuchun] #Tech spark connect mysql ok

--- CrossDataSet-Demo/src/main/java/com/tank/App.java
+++ CrossDataSet-Demo/src/main/java/com/tank/App.java
@@ -1,6 +1,8 @@
 package com.tank;
 
 import com.tank.utils.MySqlUtils;
+import com.tank.utils.SparkUtils;
+
 
 /**
  * Hello world!
@@ -8,11 +10,15 @@ import com.tank.utils.MySqlUtils;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
         MySqlUtils mySqlUtils = MySqlUtils.instance();
         /*第一次需要初始化数据库，以后运行不需要*/
         boolean needStart = false;
         mySqlUtils.initDefaultTable(needStart);
+
+        SparkUtils sparkUtils = SparkUtils.getInstance();
+        sparkUtils.create().limit(1).toDF().printSchema();
+
     }
 }
---
+++ CrossDataSet-Demo/src/main/java/com/tank/utils/SparkUtils.java
@@ -0,0 +1,47 @@
+package com.tank.utils;
+/*                                                                      *\
+**                                                                      **
+**      __  __ _________ _____          ©Mort BI                        **
+**     |  \/  / () | () |_   _|         (c) 2015                        **
+**     |_|\/|_\____|_|\_\ |_|           http://www.bigeyedata.com       **
+**                                                                      **
+\*                                                                      */
+
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.SQLContext;
+
+
+import java.util.Properties;
+
+public class SparkUtils {
+
+    public Dataset create() throws  Exception{
+        Properties prop = PropertyUtils.instance().getProperties("mysql.properties");
+
+        SparkConf conf = new SparkConf().setMaster("local").setAppName("CrossDataSet");
+        SparkContext sc = new SparkContext(conf);
+        Class.forName(prop.getProperty("mysql.driver"));
+        SQLContext sql = new SQLContext(sc);
+
+        Properties connectProps = new Properties();
+        String url = prop.getProperty("mysql.url");
+        String person = prop.getProperty("mysql.table.person");
+        connectProps.put("user", prop.getProperty("mysql.user"));
+        connectProps.put("password", prop.getProperty("mysql.password"));
+
+        return sql.read().jdbc(url, person, connectProps);
+    }
+
+    public  static SparkUtils getInstance() {
+        return instance;
+    }
+
+    private static  SparkUtils instance = new SparkUtils();
+
+    private  SparkUtils() {
+
+    }
+}
